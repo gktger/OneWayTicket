e96111aa2bf206588d4e33537fa44da5c2f53df1
Goktug Ercaliskan <goktug.ercaliskan@BK-LAB204-IS025>
2019-04-19 11:14:09 +0300

    Taslak Oluşturuldu. Entity Frame Work Eklendi

--- OneWayTicket.UI/Program.cs
+++ OneWayTicket.UI/Program.cs
@@ -16,7 +16,7 @@ namespace OneWayTicket.UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Main_Form());
         }
     }
 }
